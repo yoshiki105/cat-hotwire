811053271891d587ddf347c6b172fc60d6787f80
Yoshiki Hasumi <yoshiki.h.105@gmail.com>
2022-10-04 08:51:22 +0900

    Add: ransackで検索機能を追加.

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,7 +3,13 @@ class CatsController < ApplicationController
 
   # GET /cats
   def index
-    @cats = Cat.page(params[:page])
+    @search = Cat.ransack(params[:q])
+
+    # デフォルトのソートをid降順にする
+    @search.sorts = 'id desc' if @search.sorts.empty?
+
+    # `@search.result` で検索結果となる@catsを取得する
+    @cats = @search.result.page(params[:page])
   end
 
   # GET /cats/1
